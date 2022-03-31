3ea89012d10caa354d3a94e092a681e4e9313f15
Dalibor Králik <dadokralik@gmail.com>
2022-03-31 21:38:46 +0200

    Connection of Foreign keys in ShareRideEntity

--- TravelAgency/TravelAgency.DAL/Entities/ShareRideEntity.cs
+++ TravelAgency/TravelAgency.DAL/Entities/ShareRideEntity.cs
@@ -17,6 +17,10 @@ namespace TravelAgency.DAL.Entities
         public CarEntity? Car { get; init; }
 
         [ForeignKey(nameof(DriverId))]
+        [InverseProperty("DriverShareRides")]
+        //InverseProperty needs to be add because calss have 2 foreign keys and cant determined which one select in test
+        //https://stackoverflow.com/questions/67530126/system-invalidoperationexception-unable-to-determine-the-relationship-represent
+        //https://docs.microsoft.com/en-us/ef/core/modeling/relationships?tabs=data-annotations%2Cdata-annotations-simple-key%2Csimple-key#manual-configuration
         public UserEntity? Driver { get; init; }
 
         public ICollection<UserEntity>? Passengers { get; init; }
