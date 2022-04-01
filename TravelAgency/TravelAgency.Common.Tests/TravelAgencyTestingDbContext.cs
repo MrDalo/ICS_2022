ec04d107c2cb08f2200c7c2f7d4f2de9639d0b68
LukasChytilSK <chytil.lukas3@gmail.com>
2022-04-01 18:00:34 +0200

    Added Common.Test interface

---
+++ TravelAgency/TravelAgency.Common.Tests/TravelAgencyTestingDbContext.cs
@@ -0,0 +1,30 @@
+﻿using TravelAgency.Common.Tests.Seeds;
+using TravelAgency.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelAgency.Common.Tests
+{
+    public class CookBookTestingDbContext : TravelAgencyDbContext
+    {
+        private readonly bool _seedTestingData;
+
+        public CookBookTestingDbContext(DbContextOptions contextOptions, bool seedTestingData = false)
+            : base(contextOptions, seedDemoData: false)
+        {
+            _seedTestingData = seedTestingData;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            if (_seedTestingData)
+            {
+                //TODO
+                /* IngredientSeeds.Seed(modelBuilder);
+                 RecipeSeeds.Seed(modelBuilder);
+                 IngredientAmountSeeds.Seed(modelBuilder);*/
+            }
+        }
+    }
+}
--- TravelAgency/TravelAgency.DAL.Tests/DbContextUserTests.cs
+++ TravelAgency/TravelAgency.DAL.Tests/DbContextUserTests.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TravelAgency.DAL.Entities;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -12,6 +14,32 @@ namespace TravelAgency.DAL.Tests
         {
         }
         [Fact]
+
+
+        public async Task AddNew_User_Persisted()
+        {
+            //Arrange
+            UserEntity entity = new(
+                Guid.Parse("C5DE45D7-64A0-4E8D-AC7F-BF5CFDFB0EFC"),
+                //Id: Guid.Parse(input: "dc5df605-d676-4c25-98d8-5b795c7b6503"),
+                Name: "David",
+                Login: "xsmith00",
+                Surname: "Smith",
+                Email: "davidsmith@gmail.com",
+                PhoneNumber: "+421789564111",
+                PhotoUrl: null
+            );
+
+            //Act
+            TravelAgencyDbContextSUT.Users.Add(entity);
+            await TravelAgencyDbContextSUT.SaveChangesAsync();
+
+            //Assert
+            await using var dbx = await DbContextFactory.CreateDbContextAsync();
+            var actualEntities = await dbx.Users.SingleAsync(i => i.Id == entity.Id);
+            Assert.Equal(entity, actualEntities);
+        }
+
         public async Task GetAll_Users_FromDB()
         {
             var users = await TravelAgencyDbContextSUT.Users.ToArrayAsync();
