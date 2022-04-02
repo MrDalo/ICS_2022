8b0a605fab5b7f102ae0a6d2f6531685845e4e5d
Dalibor Králik <dadokralik@gmail.com>
2022-04-02 16:49:22 +0200

    Start of .BL

---
+++ TravelAgency/TravelAgency.BL/BusinessLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("TravelAgency.BL.Tests")]
+
+namespace TravelAgency.BL
+{
+    internal class BusinessLogic
+    {
+    }
+}
